182ce3420531571767a1fc2f2b6113942291277d
Fullerite <sh.ramil2005@gmail.com>
2025-05-08 00:27:45 +0300

    feat(gradio): add more robust error hanlding for user interactions

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from rag.embedding.embedding_model import SentenceTransformersEmbeddingModel
 from rag.generation.generation_model import TransformersGenerationModel
 from rag.rag_pipeline import RAGPipeline
 from rag_logs.logger import configure_logging
-from exceptions.custom_exceptions import ModelLoadingError, RAGInitializationError
+from exceptions.custom_exceptions import (
+    ModelLoadingError,
+    RAGInitializationError,
+    CorpusCreationError,
+    QueryProcessingError
+)
+
+
+configure_logging()
+logger = logging.getLogger(__name__)
+rag_pipeline = None
 
 
 # TODO: add the ability to re-initialize the models without re-creating the knowledge corpus
@@ -29,22 +39,34 @@ def initialize_pipeline(
         state.append("Generation model has been loaded.")
         yield " ".join(state)
     except ModelLoadingError as e:
-        return f"An unexpected error occured when loading {repr(e.model_name)}."
+        raise gr.Error(
+            message=e.message,
+            duration=None
+        )
 
     try:
         rag_pipeline = RAGPipeline(embedding_model, generation_model)
         state.append("RAG pipeline initialized.")
         yield " ".join(state)
     except RAGInitializationError as e:
-        return f"An unexpected error occured during RAG pipeline initialization."
+        raise gr.Error(
+            message=e.message,
+            duration=None
+        )
 
-    rag_pipeline.create_knowledge_corpus(
-        data_dir="data",
-        task_pattern=r"(Question\s+\d+\..*?)(?=Question\s+\d+\.|\Z)",
-        answer_pattern=r"^[A-D]\)",
-    )
-    state.append("Knowledge corpus created.")
-    yield " ".join(state)
+    try:
+        rag_pipeline.create_knowledge_corpus(
+            data_dir="data",
+            task_pattern=r"(Question\s+\d+\..*?)(?=Question\s+\d+\.|\Z)",
+            answer_pattern=r"^[A-D]\)",
+        )
+        state.append("Knowledge corpus created.")
+        yield " ".join(state)
+    except CorpusCreationError as e:
+        raise gr.Error(
+            message=e.message,
+            duration=None
+        )
 
 
 def query(
@@ -53,24 +75,43 @@ def query(
 ):
     global rag_pipeline
 
-    streamed_output, context = rag_pipeline.query(
-        user_query=user_query,
-        system_prompt=system_prompt,
-        stream_output=True
-    )
+    if rag_pipeline is not None:
+        try:
+            streamed_output, context = rag_pipeline.query(
+                user_query=user_query,
+                system_prompt=system_prompt,
+                stream_output=True
+            )
 
-    generated_text = ""
-    for text_chunk in streamed_output:
-        generated_text += text_chunk
-        yield generated_text, context
+            generated_text = ""
+            for text_chunk in streamed_output:
+                generated_text += text_chunk
+                yield generated_text, context
+        except QueryProcessingError as e:
+            raise gr.Error(
+                message=e.message,
+                duration=None
+            )
+    else:
+        raise gr.Error(
+            message="You haven't initialized the RAG pipeline yet. Proceed to the 'Load models' page, please.",
+            duration=None
+        )
 
 
-configure_logging()
-logger = logging.getLogger(__name__)
 
 # TODO: create the knowledge corpus on startup, and not on each query call
 # TODO: add file upload functionality to dynamically extend knowledge corpus ???
 # TODO: add task and answer patterns configuration for better flexibility ???
+load_models_interface = gr.Interface(
+    fn=initialize_pipeline,
+    inputs=[
+        gr.Textbox(label="Embedding Model Name"),
+        gr.Textbox(label="Generation Model Name")
+    ],
+    outputs=gr.Textbox(label="State"),
+    title="Load The Models for RAG Pipeline"
+)
 query_interface = gr.Interface(
     fn=query,
     inputs=[
@@ -86,15 +127,6 @@ query_interface = gr.Interface(
         "Ask questions about your English proficiency test and get detailed explanations.\n\n"
         "**Disclaimer**: This tool provides explanations based only on the content of the loaded PDF documents and model's raw knowledge. The embedding model might not always fetch the correct passage. AI-generated explanations may sometimes be inaccurate or incomplete. 'Source Information Used' shows the retrieved context the model used to generate the output."
     )
-)
-load_models_interface = gr.Interface(
-    fn=initialize_pipeline,
-    inputs=[
-        gr.Textbox(label="Embedding Model Name"),
-        gr.Textbox(label="Generation Model Name")
-    ],
-    outputs=gr.Textbox(label="State"),
-    title="Load The Models for RAG Pipeline"
 )
 app = gr.TabbedInterface(
     [load_models_interface, query_interface],
