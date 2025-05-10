339a404cb92eecf90658bd7e80dc3eb2196581ad
Fullerite <sh.ramil2005@gmail.com>
2025-05-10 16:12:29 +0300

    refactor(gradio): restructure UI elements

--- app/main.py
+++ app/main.py
@@ -1,62 +1,51 @@
 import gradio as gr
 from app.handlers import get_corpus_files_md, upload_file, initialize_pipeline, query
 
-
 import logging
 from rag_logs.logger import configure_logging
 configure_logging()
 logger = logging.getLogger(__name__)
 
 
-# TODO: add file upload functionality to extend knowledge corpus (a separate tab that adds the file to the data folder and maybe calls create_corpus)
-# TODO: add task and answer patterns configuration for better flexibility ???
 with gr.Blocks() as app:
     gr.Markdown("# RAG pipeline for study material Q&A")
     with gr.Tabs():
         with gr.TabItem(label="Load Models"):
-            with gr.Row():
-                gr.Markdown("### If you have uploaded new files, please reinitialize the RAG pipeline for changes to take effect.")
+            gr.Markdown("### If you have uploaded new files, please reinitialize the RAG pipeline for changes to take effect.")
             with gr.Row():
                 with gr.Column(scale=1):
-                    with gr.Row():
-                        embedding_model_tb = gr.Textbox(
-                            label="Embedding Model Name",
-                            placeholder="e.g., nomic-ai/nomic-embed-text-v1.5",
-                            value="nomic-ai/nomic-embed-text-v1.5"
-                        )
-                    with gr.Row():
-                        generation_model_tb = gr.Textbox(
-                            label="Generation Model Name",
-                            placeholder="e.g., Qwen/Qwen2.5-1.5B-Instruct",
-                            value="Qwen/Qwen2.5-1.5B-Instruct"
-                        )
+                    embedding_model_tb = gr.Textbox(
+                        label="Embedding Model Name",
+                        placeholder="e.g., nomic-ai/nomic-embed-text-v1.5",
+                        value="nomic-ai/nomic-embed-text-v1.5"
+                    )
+                    generation_model_tb = gr.Textbox(
+                        label="Generation Model Name",
+                        placeholder="e.g., Qwen/Qwen2.5-1.5B-Instruct",
+                        value="Qwen/Qwen2.5-1.5B-Instruct"
+                    )
+                    load_models_button = gr.Button(
+                        value="Initialize Pipeline",
+                        variant="primary",
+                    )
                 with gr.Column(scale=1):
                     pipeline_state_md = gr.Markdown(
                         value="**[X] Pipeline not initialized**",
                         min_height=100
                     )
                 with gr.Column(scale=2):
-                    with gr.Row():
-                        load_docs_file = gr.File(
-                            label="Extend knowledge base",
-                            file_types=[".pdf"],
-                            file_count="multiple"
-                        )
-            with gr.Row():
-                with gr.Column(scale=2):
-                    load_models_button = gr.Button(
-                        value="Initialize Pipeline",
-                        variant="primary",
+                    load_docs_file = gr.File(
+                        label="Extend knowledge base",
+                        file_types=[".pdf"],
+                        file_count="multiple"
                     )
-                with gr.Column(scale=1):
+                    upload_files_button = gr.Button(
+                        value="Upload Files",
+                        variant="secondary"
+                    )
                     docs_md = gr.Markdown(
                         value=get_corpus_files_md,
                         min_height=100
-                    )
-                with gr.Column(scale=1):
-                    upload_files_button = gr.Button(
-                        value="Upload files",
-                        variant="secondary"
                     )
             load_models_button.click(
                 fn=initialize_pipeline,
@@ -78,37 +67,32 @@ with gr.Blocks() as app:
                 )
             with gr.Row():
                 with gr.Column(scale=1):
-                    with gr.Row():
-                        system_prompt_tb = gr.Textbox(
-                            label="System Prompt",
-                            placeholder="[Optional] Guide the AI's explanation style (e.g., 'Be concise', 'Use simple language', 'Explain like I\'m a novice')",
-                            lines=10
-                        )
-                    with gr.Row():
-                        user_query_tb = gr.Textbox(
-                            label="User Query",
-                            placeholder="Enter your question here...",
-                            lines=2
-                        )
-                    with gr.Row():
-                        submit_button = gr.Button(
-                            value="Submit Query",
-                            variant="primary"
-                        )
+                    system_prompt_tb = gr.Textbox(
+                        label="System Prompt",
+                        placeholder="[Optional] Guide the AI's explanation style (e.g., 'Be concise', 'Use simple language', 'Explain like I\'m a novice')",
+                        lines=10
+                    )
+                    user_query_tb = gr.Textbox(
+                        label="User Query",
+                        placeholder="Enter your question here...",
+                        lines=2
+                    )
+                    submit_button = gr.Button(
+                        value="Submit Query",
+                        variant="primary"
+                    )
                 with gr.Column(scale=2):
-                    with gr.Row():
-                        source_info_tb = gr.Textbox(
-                            label="Source Information Used",
-                            show_copy_button=True,
-                            interactive=False
-                        )
-                    with gr.Row():
-                        model_output_tb = gr.Textbox(
-                            label="Model Output",
-                            show_copy_button=True,
-                            autoscroll=True,
-                            interactive=False
-                        )
+                    source_info_tb = gr.Textbox(
+                        label="Source Information Used",
+                        show_copy_button=True,
+                        interactive=False
+                    )
+                    model_output_tb = gr.Textbox(
+                        label="Model Output",
+                        show_copy_button=True,
+                        autoscroll=True,
+                        interactive=False
+                    )
             submit_button.click(
                 fn=query,
                 inputs=[system_prompt_tb, user_query_tb],
